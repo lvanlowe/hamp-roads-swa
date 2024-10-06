6b1c55e29d1652ab430a6f5fdf7d00ad51ed74e7
lvanlowe <lvanlowe@inrvisions.com>
2024-10-06 16:45:21 -0400

    feat:correct name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('basic-sample app is running!');
+    expect(page.getTitleText()).toEqual('hamp-roads-swa app is running!');
   });
 
   afterEach(async () => {
